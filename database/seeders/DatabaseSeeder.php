cf766d51bb4c28ce6e6152a40774eaae835578aa
farahhadijah <farahhadijah21@gmail.com>
2025-12-14 19:09:35 +0700

    Update seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,24 +2,27 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Database\Seeders\MahasiswaSeeder;
+use Database\Seeders\TempatPklSeeder;
+use Database\Seeders\PengajuanPklSeeder;
+use Database\Seeders\DokumenPengajuanSeeder;
+use Database\Seeders\VerifikasiSeeder;
+use Database\Seeders\PklSeeder;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            UserSeeder::class,
+            MahasiswaSeeder::class,
+            TempatPklSeeder::class,
+            PengajuanPklSeeder::class,
+            DokumenPengajuanSeeder::class,
+            VerifikasiSeeder::class,
+            PklSeeder::class,
         ]);
     }
 }
